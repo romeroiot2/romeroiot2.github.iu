f32a645eca51b01d516519267b1c4331ba82ebce
romeroiot2 <82291962+romeroiot2@users.noreply.github.com>
2021-04-13 15:55:06 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,9 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyDfJiJW2_AW1p-PKmt3w6h7kWPc9VmvzYM",
-  authDomain: "gilpgiotx.firebaseapp.com",
-  databaseURL: "https://gilpgiotx.firebaseio.com",
-  projectId: "gilpgiotx",
-  storageBucket: "gilpgiotx.appspot.com",
-  messagingSenderId: "362186415969",
-  appId: "1:362186415969:web:e732f8610cfdae9544ef20"
+    apiKey: "AIzaSyA7roOuiRfllx8fqjFkeSJX4scSeOZMldQ",
+    authDomain: "romeroiot2-eeeb0.firebaseapp.com",
+    projectId: "romeroiot2-eeeb0",
+    storageBucket: "romeroiot2-eeeb0.appspot.com",
+    messagingSenderId: "687784253842",
+    appId: "1:687784253842:web:3b5e4cd47fae5311b6894b"
 });
